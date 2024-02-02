32d4c5fdb12f468a4de09102df8ec5e9e7eca9ff
alam265 <113450350+alam265@users.noreply.github.com>
2024-02-02 17:01:24 +0600

    added

--- routes/routers.js
+++ routes/routers.js
@@ -1,5 +1,6 @@
 //EXTERNAL IMPORTS
 const express = require('express');
+const Books = require("../model/Books")
 
 //INTERNAL IMPORTS
 
@@ -7,5 +8,34 @@ const router = express.Router();
 
 //ADD BOOK
 
+router.get("/books", async(req, res, next)=> {
+    try {
+        const allBooks = await Books.find({})
+        res.status(200).json({
+            message: "Ok",
+            books: allBooks
+        })
+    } catch(err) {
+        res.status(404).json({
+            message: "not found"
+        })
+    }
+
+})
+
+router.get("/books/:id" , async(req ,res, next)=> {
+    try{
+        const {id} = req.params
+        const book = await Books.find({id}) 
+        res.status(200).json({
+            message: 'Ok',
+            book: book}) 
+        } catch(err) { 
+            res.status(404).json({
+                message: "not found"
+            })
+
+        }
+})
 
 module.exports = router;
